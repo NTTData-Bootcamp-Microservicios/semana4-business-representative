2e8b6ea96c2b00b1032aac6180a88e3fc545a7a1
Joffre Hermosilla Salas <joffre.hermosilla@gmail.com>
2022-05-06 05:39:37 -0500

    update kafka

--- src/main/java/nttdata/bootcamp/microservicios/representante/empresarial/MicroservicioRepresentanteEmpresarialApplication.java
+++ src/main/java/nttdata/bootcamp/microservicios/representante/empresarial/MicroservicioRepresentanteEmpresarialApplication.java
@@ -1,9 +1,12 @@
 package nttdata.bootcamp.microservicios.representante.empresarial;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 
 
@@ -15,5 +18,11 @@ public class MicroservicioRepresentanteEmpresarialApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MicroservicioRepresentanteEmpresarialApplication.class, args);
 	}
-
+	/*
+	 * @Bean CommandLineRunner commandLineRunner(KafkaTemplate<String, String>
+	 * kafkaTemplate) { return args ->{ kafkaTemplate.send(
+	 * "businessrepresentative",
+	 * "Mensaje desde microservicio REPRESENTANTE EMPRESARIAL con apache KAFKA ");
+	 * }; }
+	 */
 }
